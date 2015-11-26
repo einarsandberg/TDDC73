d20084581dd1ca5e7433ec57920e3329b2943b9b
einarsandberg <einars92@gmail.com>
2015-11-26 12:46:55 +0100

    retrieving and parsing JSON data from web now works

--- Lab3/app/src/main/java/com/example/einarsandberg/lab3/InteractiveSearcher.java
+++ Lab3/app/src/main/java/com/example/einarsandberg/lab3/InteractiveSearcher.java
@@ -9,7 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+import org.json.*;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -32,13 +32,16 @@ import android.text.TextWatcher;
 import android.content.Context;
 import android.util.*;
 import java.io.BufferedReader;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 /**
  * Created by einarsandberg on 2015-11-25.
  */
 public class InteractiveSearcher extends EditText
 {
     private static final String TAG = "InteractiveSearcher"; // for logging
+    private HashMap<Integer, ArrayList<String >> searchMap;
     Context context;
     private int searchID;
     HttpURLConnection urlConnection;
@@ -47,15 +50,21 @@ public class InteractiveSearcher extends EditText
         super(theContext);
         context = theContext;
         searchID = 0;
+        searchMap = new HashMap<Integer, ArrayList<String>>();
         init();
     }
     public InteractiveSearcher(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+        searchID = 0;
+        searchMap = new HashMap<Integer, ArrayList<String>>();
+
     }
 
     public InteractiveSearcher(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        searchID = 0;
+        searchMap = new HashMap<Integer, ArrayList<String>>();
 
     }
     private void init()
@@ -68,18 +77,15 @@ public class InteractiveSearcher extends EditText
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count)
-            {
-                String searchString = s.toString();
-                initConnection(searchString);
-                try
-                {
-                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                    String returnedText = readStream(in);
-                    Log.d(TAG, returnedText);
-                }
-                catch (Exception e)
-                {
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                try {
+                    String searchString = s.toString();
+                    String retrievedString = new NetworkClass().execute(searchString).get();
+
+                    parseObject(retrievedString);
+
+                } catch (Exception e) {
                     Log.d(TAG, "Error reading from stream");
                 }
 
@@ -92,45 +98,72 @@ public class InteractiveSearcher extends EditText
             }
         });
     }
-
-    private void initConnection(String theSearchString)
+    private void parseObject(String retrievedString)
     {
-
         try
         {
-            URL url = new URL("http://flask-afteach.rhcloud.com/getnames/" + searchID + "/" + theSearchString);
-            searchID++;
-            Log.d(TAG, url.toString());
-            urlConnection = (HttpURLConnection) url.openConnection();
+            Log.d(TAG, retrievedString);
+            JSONObject jsonObj = new JSONObject(retrievedString);
 
-        }
-        catch(Exception e)
-        {
-            Log.d(TAG, "Error initializing connection");
-            e.printStackTrace();
-        }
-
-    }
-    private String readStream(InputStream in)
-    {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in), 1000);
-        try
-        {
-            for (String line = bufferedReader.readLine(); line != null; line =bufferedReader.readLine())
+            JSONArray results = jsonObj.getJSONArray("result");
+            ArrayList<String> names = new ArrayList<String>();
+            for (int i = 0; i < results.length(); i++)
             {
-                sb.append(line);
+                names.add(results.get(i).toString());
             }
-            in.close();
-
+            searchMap.put(searchID, names);
         }
         catch(Exception e)
         {
-            Log.d(TAG, "Error reading stream");
+            Log.d(TAG, "Error parsing JSON");
             e.printStackTrace();
         }
-        return sb.toString();
+
+
+
+
     }
+    public class NetworkClass extends AsyncTask<String, Void, String>
+    {
+        protected String doInBackground(String... theSearchString)
+        {
+            String returnedText = "";
+            try {
+                URL url = new URL("http://flask-afteach.rhcloud.com/getnames/" + searchID + "/" + theSearchString[0]);
+                searchID++;
+                Log.d(TAG, url.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                returnedText = readStream(in);
+                urlConnection.disconnect();
+
+
+            } catch (Exception e) {
+                Log.d(TAG, "Error initializing connection");
+                e.printStackTrace();
+            }
+            return returnedText;
+
+        }
+
+        private String readStream(InputStream in) {
+            StringBuilder sb = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in), 1000);
+            try {
+
+                for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+                    sb.append(line);
+                }
+                in.close();
+
+            } catch (Exception e) {
+                Log.d(TAG, "Error reading stream");
+                e.printStackTrace();
+            }
+            return sb.toString();
+        }
+    }
+
 
 
 }
